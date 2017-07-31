4e343398293d5700b1445b247ced59c4d379e208
chengshaolian <chengshaolian@xiaomi.com>
2017-07-31 12:06:03 +0800

    modify controls view

--- mivideoplayer/src/main/java/com/mitv/videoplayer/TvControlsView.java
+++ mivideoplayer/src/main/java/com/mitv/videoplayer/TvControlsView.java
@@ -3,7 +3,11 @@ package com.mitv.videoplayer;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.mitv.videoplayer.videoview.IVideoView;
@@ -12,8 +16,12 @@ import com.mitv.videoplayer.videoview.IVideoView;
  * Created by chengsl on 7/28/17.
  */
 
-public class TvControlsView extends RelativeLayout {
+public class TvControlsView extends RelativeLayout implements View.OnClickListener {
+    private static final String TAG = "TvControlsView";
     private IVideoView mVideoView;
+    private ProgressBar mProgressBar;
+    private Button mPlayButton;
+    private Button mPauseButton;
 
     public TvControlsView(Context context) {
         this(context, null);
@@ -36,17 +44,75 @@ public class TvControlsView extends RelativeLayout {
 
     private void initViews(Context context) {
         LayoutInflater.from(context).inflate(R.layout.tv_controls_view, this, true);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setProgress(0);
+        mProgressBar.setMax(0);
+        mPlayButton = (Button) findViewById(R.id.play);
+        mPlayButton.setOnClickListener(this);
+        mPauseButton = (Button) findViewById(R.id.pause);
+        mPauseButton.setOnClickListener(this);
     }
 
     public void setVideoView(IVideoView videoView) {
         mVideoView = videoView;
+        int duration = mVideoView.getDuration() / 1000;
+        Log.d(TAG, "setVideoView: " + videoView + ", " + duration);
+        mProgressBar.setMax(duration);
+        updateProgress();
     }
 
     public void pause() {
-
+        Log.d(TAG, "pause");
+        if (mVideoView != null && mVideoView.canPause()) {
+            mVideoView.pause();
+        }
     }
 
     public void resume() {
+        Log.d(TAG, "resume");
+        if (mVideoView != null && !mVideoView.isPlaying()) {
+            mVideoView.start();
+            updateProgress();
+        }
+    }
 
+    private void updateProgress() {
+        if (mVideoView != null) {
+            if (mVideoView.isPlaying()) {
+                post(mUpdateProgress);
+            } else {
+                postDelayed(mUpdateProgress, 1000);
+            }
+        }
+    }
+
+    private Runnable mUpdateProgress = new Runnable() {
+        @Override
+        public void run() {
+//            if (mVideoView != null && mVideoView.isPlaying()) {
+                if (mProgressBar.getMax() == 0) {
+                    int duration = mVideoView.getDuration() / 1000;
+                    Log.d(TAG, "mUpdateProgress: duration " + duration);
+                    mProgressBar.setMax(duration);
+                }
+                int progress = mVideoView.getCurrentPosition() / 1000;
+//                Log.d(TAG, "mUpdateProgress: " + progress);
+                mProgressBar.setProgress(progress);
+                postDelayed(mUpdateProgress, 1000);
+//            }
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick: " + view);
+        switch (view.getId()) {
+            case R.id.play:
+                resume();
+                break;
+            case R.id.pause:
+                pause();
+                break;
+        }
     }
 }
